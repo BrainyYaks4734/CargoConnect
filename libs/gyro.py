ddab73724a646d76697f18d5e0c112c4d898208e
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-09-09 16:25:55 -0400

    Update gyro.py

--- libs/gyro.py
+++ libs/gyro.py
@@ -1,24 +1,15 @@
-from math import *
-
-motors = MotorPair('A', 'B')
-motors.set_default_speed(100)
-def gyroCW(degrees):
-    hub.motion_sensor.reset_yaw_angle()
-    angle = hub.motion_sensor.get_yaw_angle()
-    motors.start(100,20)
-    while angle<degrees:
-        angle = hub.motion_sensor.get_yaw_angle()
-    motors.stop()
-def gyroCCW(degrees):
-    hub.motion_sensor.reset_yaw_angle()
-    angle = hub.motion_sensor.get_yaw_angle()
-    motors.start(-100,20)
-    while angle>degrees:
-        angle = hub.motion_sensor.get_yaw_angle()
-    motors.stop()
-
-def gyro(degrees):
+def gyroTurn(degrees, speed):
     if (degrees < 0):
-        gyroCCW(degrees)
+        hub.motion_sensor.reset_yaw_angle()
+        angle = hub.motion_sensor.get_yaw_angle()
+        motors.start(-100,speed)
+        while angle>degrees:
+            angle = hub.motion_sensor.get_yaw_angle()
+        motors.stop()
     else:
-        gyroCW(degrees)
+        hub.motion_sensor.reset_yaw_angle()
+        angle = hub.motion_sensor.get_yaw_angle()
+        motors.start(100, speed)
+        while angle<degrees:
+            angle = hub.motion_sensor.get_yaw_angle()
+        motors.stop()
